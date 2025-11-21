a3d4d87ac4353963d7b0a28f824a85d5ba8c5543
joshua <bsit2c2025@gmail.com>
2025-11-21 15:54:08 +0800

    adding script

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("services_button").addEventListener("click", function() {
+alert("Services!");
+});
+
+document.getElementById("contacts_button").addEventListener("click", function() {
+alert("Contacts!");
+});
